778007c7c6baa411ab128e3b8b68c66133245775
Evan Freymiller <evan.freymiller@procore.com>
2020-07-30 10:27:20 -0400

    state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [isHappyHourCanceled, setIsHappyHourCanceled] = React.useState(false);
+
   const title = "Happy Hour Indicator";
 
   const currentHour = new Date().getHours();
@@ -12,17 +14,27 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>{title}</h1>
-        <DetermineHappyHour currentHour={currentHour} name="Evan" />
+        <DetermineHappyHour
+          currentHour={currentHour}
+          name="Evan"
+          isHappyHourCanceled={isHappyHourCanceled}
+        />
+        <button onClick={() => setIsHappyHourCanceled(!isHappyHourCanceled)}>
+          {isHappyHourCanceled ? "Reinstate Happy Hour" : "Cancel Happy Hour"}
+        </button>
       </header>
     </div>
   );
 }
 
 const DetermineHappyHour = (props) => {
+  if (props.isHappyHourCanceled === true) {
+    return <div>Happy Hour is canceled.</div>;
+  }
   if (props.currentHour >= 17) {
     return <div>It's time for Happy Hour {props.name}!</div>;
   }
-  return <div>It's not time for Happy Hour {props.name}</div>;
+  return <div>It's not time for Happy Hour {props.name}.</div>;
 };
 
 export default App;
