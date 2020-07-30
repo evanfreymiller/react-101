142545c30010be4327bd4f1956e58565efdc8bf2
Evan Freymiller <evan.freymiller@procore.com>
2020-07-30 09:53:36 -0400

    removed old function

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ import "./App.css";
 function App() {
   const happyHour = false;
 
-  const isHappyHour = () => {
-    if (happyHour) {
-      return "It's time for Happy Hour!";
-    }
-    return "It's not time for Happy Hour";
-  };
-
   return (
     <div className="App">
       <header className="App-header">
