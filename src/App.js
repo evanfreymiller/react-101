4561cf3e584dd8fef88f791331e8758304dda547
Evan Freymiller <evan.freymiller@procore.com>
2020-07-30 09:56:11 -0400

    added title const

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 function App() {
   const happyHour = false;
+  const title = "React 101";
 
   const isHappyHour = () => {
     if (happyHour) {
@@ -16,7 +17,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>React 101</h1>
+        <h1>{title}</h1>
         {isHappyHour()}
       </header>
     </div>
