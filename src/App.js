213660d55e8e0d00eff7a8c61a747a09552e3839
Evan Freymiller <evan.freymiller@procore.com>
2020-07-30 09:51:02 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const happyHour = false;
+
+  const isHappyHour = () => {
+    if (happyHour) {
+      return "It's time for Happy Hour!";
+    }
+    return "It's not time for Happy Hour";
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React 101</h1>
+        {happyHour ? <HappyHour /> : <NotHappyHour />}
       </header>
     </div>
   );
+}
+
+const HappyHour = () => {
+  return (
+    <div>It's time for Happy Hour!</div>
+  )
+}
+
+const NotHappyHour = () => {
+  return (
+    <div>It's not time for Happy Hour</div>
+  )
 }
 
 export default App;
